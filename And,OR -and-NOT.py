cc474033e31f47b0d6960d78e6381ec371eeacb6
aadhyagupta20-del <aadhyagupta20@gmail.com>
2025-12-31 13:50:44 +0400

    AND,OR-and-NOT.py

---
+++ And,OR -and-NOT.py
@@ -0,0 +1,15 @@
+a=30
+b=-4
+c=0
+if(a and b and c):
+    print("The number boolian is True")
+else:
+    print("It is false")
+
+d=12
+e=-54
+f=0
+if(a>0 or b>0):
+    print("The number is greater then 0")
+else:
+    print("The number is smaller the 0")
