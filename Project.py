700d8ed441b85b85517e45afd6daac91b833ec53
aadhyagupta20-del <aadhyagupta20@gmail.com>
2026-01-06 18:55:35 +0400

    project.py

---
+++ Project.py
@@ -0,0 +1,10 @@
+c = input("Enter a character: ")
+if len(c) != 1:
+    print("Please enter exactly one character.")
+elif '0' <= c <= '9':
+    print("It is a number.")
+elif 'A' <= c <= 'Z' or 'a' <= c <= 'z':
+    print("It is a letter.")
+else:
+    print("It is neither a number nor a letter.")
+print("Homework done.")
